ee357c7194e388dd96558c071d13e020044c88e9
ShahriarArefin <arfin17310@gmail.com>
2024-08-17 04:13:20 +0600

    added api to add a product and get details of a product

--- server/src/controllers/Product.ts
+++ server/src/controllers/Product.ts
@@ -2,6 +2,17 @@ import { Request, Response } from "express";
 import { ErrorResponse, request } from "../utils";
 import Product from "../models/Product";
 
+// create a new product
+export const create = request(async (req: Request, res: Response) => {
+   const product = await Product.create(req.body);
+
+   res.status(201).send({
+      success: true,
+      message: "Product added successfully",
+      data: product,
+   });
+});
+
 // upload multiple products
 export const uploadMany = request(async (req: Request, res: Response) => {
    const products = req.body;
@@ -14,4 +25,11 @@ export const uploadMany = request(async (req: Request, res: Response) => {
       success: true,
       message: "Products uploaded successfully",
    });
+});
+
+// get details of a product
+export const get = request(async (req: Request, res: Response) => {
+   const product = await Product.findById(req.params.id).select("-updatedAt");
+   if (!product) throw new ErrorResponse(404, "Product not found");
+   res.status(200).send(product);
 });
--- server/src/routes/productRoute.ts
+++ server/src/routes/productRoute.ts
@@ -3,7 +3,13 @@ import * as Product from "../controllers/Product";
 
 const router = express.Router();
 
+// create a new product
+router.post("/", Product.create);
+
 // upload multiple products
 router.post("/upload", Product.uploadMany);
+
+// get products details
+router.get("/:id", Product.get);
 
 export default router;
