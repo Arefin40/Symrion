5f96844ec1260a18518bbecd69625f50efe23629
ShahriarArefin <arfin17310@gmail.com>
2024-08-17 19:26:48 +0600

    added homepage

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,3 +1,24 @@
 export default function Home() {
-   return <>Homepage</>;
+   return (
+      <section className="mb-8 lg:mb-12 space-y-8 lg:space-y-12">
+         <header>
+            <img src="https://res.cloudinary.com/arefin40/image/upload/v1723859572/symrion/c6abqhhisuwatjdsgb9g.webp" />
+         </header>
+
+         <section className="container grid grid-cols-[repeat(auto-fit,minmax(16rem,20rem))] justify-center gap-6">
+            <div className="lg:p-10 aspect-square rounded-full bg-[#ecfcfc]">
+               <img src="https://res.cloudinary.com/arefin40/image/upload/v1723860697/symrion/cabizsrrkoqctmxwyqnq.png" />
+            </div>
+            <div className="lg:p-10 aspect-square rounded-full bg-[#fff3f1]">
+               <img src="https://res.cloudinary.com/arefin40/image/upload/v1723860697/symrion/g8p6y7tamlkoje30azku.png" />
+            </div>
+            <div className="lg:p-10 aspect-square rounded-full bg-[#e7f1ff]">
+               <img src="https://res.cloudinary.com/arefin40/image/upload/v1723860697/symrion/cr8lgqj0ttocz2jnebwx.png" />
+            </div>
+            <div className="lg:p-10 aspect-square rounded-full bg-[#fff7e5]">
+               <img src="https://res.cloudinary.com/arefin40/image/upload/v1723860696/symrion/eqqzh8mlpfhenwffcbqd.png" />
+            </div>
+         </section>
+      </section>
+   );
 }
